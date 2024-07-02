811a0ecf8f3befc53897fe37f56e6b5b8178c3c4
Soumyadip Sarkar <soumyadip@soumyadipsarkar.in>
2024-07-02 21:07:06 +0530

    new

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "Kolkata",
+    country: "IN",
+    state: "West Bengal",
+    lat: 22.5744,
+    lon: 88.3629,
+  },
+  {
+    name: "Bengaluru",
+    country: "IN",
+    state: "Karnataka",
+    lat: 12.9716,
+    lon: 77.5946,
+  },
+  {
+    name: "Mumbai",
+    country: "IN",
+    state: "Maharashtra",
+    lat: 19.076,
+    lon: 72.8777,
+  },
+  {
+    name: "New Delhi",
+    country: "IN",
+    state: "Delhi",
+    lat: 28.6139,
+    lon: 77.2088,
+  },
+  {
+    name: "Hyderabad",
+    country: "IN",
+    state: "Telangana",
+    lat: 17.4065,
+    lon: 78.4772,
+  },
+];
+
+export default defaultStates;
